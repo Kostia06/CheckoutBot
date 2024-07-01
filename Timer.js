dd4053caf03d8db9a9012cb70b76e70817f14f1b
Kostia06 <ilnkostia@gmail.com>
2024-07-01 10:28:42 -0600

    error handling

--- Timer.js
+++ Timer.js
@@ -1,17 +1,8 @@
 
 class Timer{
     constructor(){
-        this.time = new Date();
     }
 
-    setTimer(month, day, hour, minute){
-        this.time = new Date();
-        this.time.setMonth(month);
-        this.time.setDate(day);
-        this.time.setHours(hour);
-        this.time.setMinutes(minute);
-        this.time.setSeconds(0);
-    }
 
     delay(){
         return new Promise(
@@ -19,13 +10,12 @@ class Timer{
         );
     }
 
-    async wait(){
+    async wait(date, minutes = 0){ 
         let currentTime = new Date();
-        while(currentTime < this.time){
+        date.setMinutes(date.getMinutes() - minutes);
+        while(currentTime < date){
             await this.delay();
             currentTime = new Date();
-            console.log("Current time: " + currentTime);
-            console.log("Execution time: " + this.time);
         }
     }
 }
--- index.js
+++ index.js
@@ -18,14 +18,17 @@ const months = [
 ];
 
 class Main{
-    constructor(){
+        constructor(){
+        this.date = new Date();
         this.dates = []
-        let today = new Date();
         for(let i = 0; i < 3; i++){
-            this.dates.push(months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear());
+            let str = months[this.date.getMonth()] + " " + this.date.getDate() + ", " + this.date.getFullYear();
+            this.dates.push(str);
+            this.date.setDate(this.date.getDate() + 1);
         }
         this.timer = new Timer();
         this.bb = new BrowserBot();
+        this.dateToExecute = new Date();
     }
 
     async setTimer(month, day, hour, minute){
@@ -33,76 +36,82 @@ class Main{
     }
 
     async run(){
-        await this.timer.wait();
-        if(this.bb == null){
-            console.log("BrowserBot not initialized");
-            return;
+        while(true){
+            this.dateToExecute.setHours(9);
+            this.dateToExecute.setMinutes(15);
+            this.dateToExecute.setSeconds(0);
+            console.log("Waiting for "+ this.dateToExecute);
+            await this.timer.wait(this.dateToExecute, 2);
+            await this.bb.initBrowser(); 
+            await this.login();
+            await this.gotoPage();
+            await this.timer.wait(this.dateToExecute);
+            await this.bookOnDate();
+            await this.bb.closeBrowser();
+            this.dateToExecute.setDate(this.dateToExecute.getDate() + 1);
         }
-        await this.bb.initBrowser(); 
-        await this.login();
-        await this.gotoPage();
-        await this.timer.wait();
-        await this.bookOnDate();
-        await this.bb.closeBrowser();
     }
 
     async login(){
-        await bb.gotoPage("https://anc.apm.activecommunities.com/santamonicarecreation/signin?onlineSiteId=0&from_original_cui=true&override_partial_error=False&custom_amount=False&params=aHR0cHM6Ly9hcG0uYWN0aXZlY29tbXVuaXRpZXMuY29tL3NhbnRhbW9uaWNhcmVjcmVhdGlvbi9BY3RpdmVOZXRfSG9tZT9GaWxlTmFtZT1hY2NvdW50b3B0aW9ucy5zZGkmZnJvbUxvZ2luUGFnZT10cnVl");
-        await bb.setInput("input", "aria-label='Login name Required'", user);
-        await bb.setInput("input", "aria-label='Password Required'", password);
-        await bb.clickButton("button", "type='submit'");
+        await this.bb.gotoPage("https://anc.apm.activecommunities.com/santamonicarecreation/signin?onlineSiteId=0&from_original_cui=true&override_partial_error=False&custom_amount=False&params=aHR0cHM6Ly9hcG0uYWN0aXZlY29tbXVuaXRpZXMuY29tL3NhbnRhbW9uaWNhcmVjcmVhdGlvbi9BY3RpdmVOZXRfSG9tZT9GaWxlTmFtZT1hY2NvdW50b3B0aW9ucy5zZGkmZnJvbUxvZ2luUGFnZT10cnVl");
+        await this.bb.setInput("input", "aria-label='Login name Required'", user);
+        await this.bb.delay();
+        await this.bb.setInput("input", "aria-label='Password Required'", password);
+        await this.bb.delay();
+        await this.bb.clickButton("button", "type='submit'");
     }
 
     async gotoPage(){
-        await bb.gotoPage("https://anc.apm.activecommunities.com/santamonicarecreation/reservation/landing/quick?groupId=4");
-        await bb.delay();
-        await bb.setInput("input", "aria-label='Event name'", "Casual Tennis");
+        await this.bb.gotoPage("https://anc.apm.activecommunities.com/santamonicarecreation/reservation/landing/quick?groupId=4");
+        await this.bb.delay();
+        await this.bb.setInput("input", "aria-label='Event name'", "Casual Tennis");
+        await this.bb.delay();
     }
 
     async bookOnDate(){
-        for(let date of dates){
-            console.log("Checking for open slots on "+date);
+        for(let date of this.dates){
             // selects date
-            await bb.clickButton("div", "class='an-input-addon'");
-            let calendar = await bb.getInnerText("span", "class='an-calendar-header-title an-calendar-header-title--disabled'");
-            console.log("Current month: "+calendar);
-            
+            await this.bb.clickButton("div", "class='an-input-addon'");
+            await this.bb.delay();
+            let calendar = await this.bb.getInnerText("span", "class='an-calendar-header-title an-calendar-header-title--disabled'");
+            await this.bb.delay(); 
             // checks if the date is in the current month
             if(!calendar.includes(date.split(" ")[0])){
-                await bb.clickButton("i", "aria-label='Switch calendar to next month right arrow'");
+                await this.bb.clickButton("i", "aria-label='Switch calendar to next month right arrow'");
             }
-
             // selects date
-            await bb.clickButton("div", `aria-label='${date}'`);
-            await this.book();
+            await this.bb.clickButton("div", `aria-label='${date}'`);
+            let found = await this.book(date);
+            if(found) break;
         }
     }
 
-    async book(){
+    async book(date){
         // checks for any open slots
         let i = 0;
         while(i != -1){
             let j = 0;
             while(true){
-                console.log("\tChecking slot "+i+"-"+j);
-                const available = await bb.getAttribute("div", `aria-activedescendant='cell-${i}-${j}'`, "aria-label");
+                const available = await this.bb.getAttribute("div", `aria-activedescendant='cell-${i}-${j}'`, "aria-label");
                 if(!available){
                     if(j == 0){ i = -2;}
                     break;
                 }
                 if(available.includes("Avaiable")){
                     console.log("Found an open slot on "+date+" at "+ available);
-                    await bb.clickButton("button", "type='submit'");
-                    break;
+                    await this.bb.clickButton("button", "type='submit'");
+                    return true;
                 }
                 j++;
             }
             i++;
         }
+        return false;
     }
 
     
 }
 
 const main = new Main();
-main.run();
+await main.run();
+rl.close();
